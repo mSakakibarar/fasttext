9c02ae5df93dfb2890d2ede980ee1dbb8ab6cee9
sakaki <sakaki@sakakinoMacBook-Air.local>
2019-09-01 19:34:45 +0900

    パラメータで受け取った文字をカテゴリ分類する。

--- src/urls.py
+++ src/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('category1', views.Category1.as_view(), name='category1'),
-    path('category2', views.Category2.as_view(), name='category2'),
+    path('category1/<str:text>', views.Category1.as_view(), name='category1'),
+    path('category2/<str:text>', views.Category2.as_view(), name='category2'),
 ]
--- src/views.py
+++ src/views.py
@@ -5,11 +5,11 @@ from rest_framework.response import Response
 import fasttext as ft
 
 class Category1(APIView):
-    def get(self, request, format=None):
+    def get(self, request, text, format=None):
         classifier = ft.load_model("data/results/result.bin")
-        return Response({"category1": classifier.predict("分類する文字1")})
+        return Response({"category1": classifier.predict(text)})
 
 class Category2(APIView):
-    def get(self, request, format=None):
+    def get(self, request, text, format=None):
         classifier = ft.load_model("data/results/result.bin")
-        return Response({"category1": classifier.predict("分類する文字2")})
+        return Response({"category1": classifier.predict(text)})
