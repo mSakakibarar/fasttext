90576f75c76728ea691f3af7cbe97917b90ce18a
sakaki <sakaki@sakakinoMacBook-Air.local>
2019-09-01 19:19:02 +0900

    固定の文字列を分類する。

--- src/views.py
+++ src/views.py
@@ -2,11 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+import fasttext as ft
 
 class Category1(APIView):
     def get(self, request, format=None):
-        return Response({"category1": "カテゴリーを返す"})
+        classifier = ft.load_model("data/results/result.bin")
+        return Response({"category1": classifier.predict("分類する文字1")})
 
 class Category2(APIView):
     def get(self, request, format=None):
-        return Response({"category2": "カテゴリーを返す"})
+        classifier = ft.load_model("data/results/result.bin")
+        return Response({"category1": classifier.predict("分類する文字2")})
